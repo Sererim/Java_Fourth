8b87f5bd3bc6e0c7ee1d33e765165e3aac344c66
Sererim <danielleymanw@gmail.com>
2023-02-16 22:32:36 +0300

    Bugfix. Replaced if if if else with switch statment.

--- Console.java
+++ Console.java
@@ -9,25 +9,35 @@ public class Console{
     {
         String foo = "";
         LinkedList<String> list = new LinkedList<String>();
-        System.out.println("Program reads and saves entered text.\nTo print out saved text enter print");
+        System.out.println("Program reads, deletes and saves entered text.\nTo print out saved text enter print.\nTo delete previously entered word enter revert.");
         System.out.println("To terminate the program enter exit");
         while(true)
         {
             foo = scan.nextLine();
-            if(foo.equals("print") && list.size() != 0)
-            {
-                for(int i = list.size() - 1; i >= 0; i--)
-                    System.out.print(list.get(i) + " ");
-                System.out.println();
-            }
-            if(foo.equals("exit"))
-                break;
-            else
-            {
-                list.addLast(foo);
+            switch (foo) {
+                case "print":
+                    if(list.size() != 0)
+                        {
+                        for(int i = list.size() - 1; i >= 0; i--)
+                            System.out.print(list.get(i) + " ");
+                        System.out.println();
+                        break;
+                        }
+                    continue;
+                case "revert":
+                    if(list.size() != 0)
+                    {
+                        list.removeLast();
+                        break;
+                    }
+                    continue;
+                case "exit":
+                    return true;
+                default:
+                    list.addLast(foo);
+                    break;
             }
         }
-        return true;
     }
 
     public static void main(String[] args) {
